c19f24195a40552072bef8da6d3e312a383ad0de
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-11-26 13:56:41 +0700

    update

---
+++ src/DataService/chuyenNhiPhan.java
@@ -0,0 +1,27 @@
+//[Mã câu hỏi (qCode): QxQtOQY5].  Một dịch vụ web (hỗ trợ SOAP version 1.1) được định nghĩa và mô tả trong tệp DataService?wsdl, được triển khai trên server tại URL http://<Exam_IP>:8080/JNPWS/DataService?wsdl để xử lý các bài toán với dữ liệu nguyên thủy.
+//Yêu cầu: Viết chương trình tại máy trạm (WS client) để giao tiếp với DataService thực hiện các công việc sau:
+//a. Triệu gọi phương thức getData với tham số đầu vào là mã sinh viên (studentCode) và mã câu hỏi (qCode) để nhận về một danh sách số nguyên (List<Integer>) từ server.
+//b. Chuyển đổi số nguyên nhận được từ hệ thập phân sang hệ nhị phân và biểu diễn kết quả dưới dạng chuỗi nhị phân.
+//c. Triệu gọi phương thức submitDataStringArray(String studentCode, String qCode, List<String> data) để gửi chuỗi nhị phân đã chuyển đổi trở lại server.
+//Ví dụ: Nếu mỗi số nguyên nhận được từ phương thức getData, chương trình client sẽ chuyển đổi sang chuỗi nhị phân là "1010", và gửi mảng chuỗi này trở lại server qua phương thức submitData.
+//d. Kết thúc chương trình client.
+package DataService;
+import vn.medianews.*;
+import java.util.*;
+public class chuyenNhiPhan {
+    public static void main(String[] args) throws Exception{
+        String studentCode = "B22DCCN008",qCode = "QxQtOQY5";
+        DataService_Service service = new DataService_Service();
+        DataService sv = service.getDataServicePort();
+        List<Integer> a = sv.getData(studentCode, qCode);
+        List<String> kq = new ArrayList<>();
+        System.out.println(a);
+        String bi = "";
+        for(int x : a){
+            bi = Integer.toBinaryString(x);
+            kq.add(bi);
+        }
+        System.out.println(kq);
+        sv.submitDataStringArray(studentCode, qCode, kq);
+    }
+}
